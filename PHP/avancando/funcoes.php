0178134624b6fc3610573a336d55d6f76feee556
jardeldias <jardel.g.dias@gmail.com>
2023-01-06 20:40:46 -0300

    os progressos de hoje

---
+++ PHP/avancando/funcoes.php
@@ -0,0 +1,37 @@
+<?php
+
+function exibeMensagem(string $mensagem)
+{
+    echo $mensagem . PHP_EOL;
+}
+
+function depositar(array $conta, float $valorADepositar): array
+{
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Depositos precisam ser positivos");
+    }
+    return $conta;
+} 
+
+function sacar(array $conta, float $valorASacar): array
+{
+    if ($valorASacar > $conta['saldo']) {
+        exibeMensagem("Você não tem saldo suficiente");
+    } else {
+        $conta['saldo'] -= $valorASacar;
+    }
+
+    return $conta;
+}
+
+/* Passagem por referência é muito perigoso,
+muita liberdade para se alterar uma variável,
+tornando mais imprevisível a cada uso.
+uso apenas quando é realmente benéfico
+*/
+function titularCaixaAlta(array &$conta)
+{
+    $conta['titular'] = strtoupper($conta['titular']);
+}  
---
+++ PHP/avancando/tipos-chaves.php
@@ -0,0 +1,5 @@
+<?php
+/* php só trabalha con Indices do tipo Inteiro ou String
+qualquer outro tipo, ele vai tentar converter para um dos
+anteriores.
+*/
